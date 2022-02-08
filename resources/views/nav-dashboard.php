55596c2f501db3643b279d32d7e99549b08fcdef
diolan12 <dio_lantief21@outlook.com>
2022-02-08 18:44:24 +0700

    Update nav-dashboard.php

--- resources/views/nav-dashboard.php
+++ resources/views/nav-dashboard.php
@@ -45,17 +45,14 @@
             <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
         </div>
     </li>
-    <li>
-        <div class="divider"></div>
-    </li>
     <li><a class="subheader">Data</a></li>
-    <li><a href="<?= rootDashboard() ?>" <?php if ($nav['active'] == 'stats') echo 'class="active"' ?>><i class="material-icons">dashboard</i>Statistics</a></li>
-    <li><a href="<?= rootDashboard('product') ?>" <?php if ($nav['active'] == 'product') echo 'class="active"' ?>><i class="material-icons">inventory_2</i>Products</a></li>
-    <li><a href="<?= rootDashboard('user') ?>" <?php if ($nav['active'] == 'user') echo 'class="active"' ?>><i class="material-icons">group</i>Users</a></li>
+    <li><a href="<?= rootDashboard() ?>" class="waves-effect <?php if ($nav['active'] == 'stats') echo 'active' ?>"><i class="material-icons">dashboard</i>Statistics</a></li>
+    <li><a href="<?= rootDashboard('product') ?>" class="waves-effect <?php if ($nav['active'] == 'product') echo 'active' ?>"><i class="material-icons">inventory_2</i>Products</a></li>
+    <li><a href="<?= rootDashboard('user') ?>" class="waves-effect <?php if ($nav['active'] == 'user') echo 'active' ?>"><i class="material-icons">group</i>Users</a></li>
     <li>
         <div class="divider"></div>
     </li>
     <li><a class="subheader">Brand</a></li>
-    <li><a class="waves-effect" href="#!" <?php if ($nav['active'] == 'about') echo 'class="active"' ?>><i class="material-icons">info</i>About</a></li>
-    <li><a class="waves-effect" href="#!" <?php if ($nav['active'] == 'setting') echo 'class="active"' ?>><i class="material-icons">settings</i>Settings</a></li>
+    <li><a class="waves-effect <?php if ($nav['active'] == 'about') echo 'active' ?>" href="#!" <?php if ($nav['active'] == 'about') echo 'class="active"' ?>><i class="material-icons">info</i>About</a></li>
+    <li><a class="waves-effect <?php if ($nav['active'] == 'setting') echo 'active' ?>" href="#!"><i class="material-icons">settings</i>Settings</a></li>
 </ul>
