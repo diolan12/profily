aac33c959199ba43527d3d9b80306cfad3d93777
diolan12 <dio_lantief21@outlook.com>
2022-02-10 14:19:03 +0700

    Update Dashboard.php

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -24,22 +24,23 @@ class Dashboard extends BaseViewController
         if ($ref == url() . rootAuth()) {
             $this->toast('Welcome ' . $request->user()->name);
         }
-        $this->setupWhatssap('Chat with developer', 'https://wa.me/6285648535927');
+        $this->setupWhatsapp('Chat with developer', 'https://wa.me/6285648535927');
     }
 
-    public function stat()
+    public function stat(Request $request)
     {
+        $this->extra['server']['client']['refresh'] = $request->input('refresh', false);
         $this->load(['visitor', 'view']);
         $this->extra['meta']['title'] = 'Statistics';
         $this->extra['nav']['active'] = 'stats';
         $this->extra['content']['main'] = 'dashboard.stats';
 
         $this->data['visitors_today'] = $this->visitor->whereDate('created_at', Carbon::today())->first();
-        $this->data['visitors_month'] = $this->visitor->whereMonth('created_at', Carbon::now('UTC')->month)->get();
-        $this->data['visitors_year'] = $this->visitor->whereYear('created_at', Carbon::now('UTC')->year)->get();
+        $this->data['visitors_month'] = $this->visitor->whereMonth('created_at', Carbon::now()->month)->get();
+        $this->data['visitors_year'] = $this->visitor->whereYear('created_at', Carbon::now()->year)->get();
         
         $this->data['views_today'] = $this->view->with($this->view->getRelations())->whereDate('created_at', Carbon::today())->orderBy('count', 'DESC')->get();
-        $this->data['views_month'] = $this->view->with($this->view->getRelations())->whereMonth('created_at', Carbon::now('UTC')->month)->get();
+        $this->data['views_month'] = $this->view->with($this->view->getRelations())->whereMonth('created_at', Carbon::now()->month)->orderBy('count', 'DESC')->get();
 
         return $this->bootstrap(true);
     }
