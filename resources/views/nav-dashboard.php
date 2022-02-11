539feca6992648a07a5ecd7fddabcbd1b32b7bcb
diolan12 <dio_lantief21@outlook.com>
2022-02-11 09:55:32 +0700

    Update nav-dashboard.php

--- resources/views/nav-dashboard.php
+++ resources/views/nav-dashboard.php
@@ -2,14 +2,15 @@
     <!-- Dropdown Structure -->
     <ul id="more" class="dropdown-content">
         <li><a href="<?= root() ?>"><i class="material-icons left">public</i>View Page</a></li>
-        <?php if ($nav['active'] == 'stats') : ?>
 
+        <?php if ($nav['active'] == 'stats') : ?>
             <?php if (!$server['client']['refresh']) : ?>
                 <li><a href="<?= rootDashboard('?refresh=30') ?>"><i class="material-icons left">check_box_outline_blank</i>Auto Refresh</a></li>
             <?php elseif ($server['client']['refresh']) : ?>
                 <li><a href="<?= rootDashboard() ?>"><i class="material-icons left">check_box</i>Auto Refresh</a></li>
             <?php endif; ?>
         <?php endif; ?>
+
         <li class="divider"></li>
         <li><a href="<?= rootAuth('logout') ?>"><i class="material-icons left">logout</i>Logout</a></li>
     </ul>
@@ -41,7 +42,11 @@
     <li><a class="subheader">Data</a></li>
     <li><a href="<?= rootDashboard() ?>" class="waves-effect <?php if ($nav['active'] == 'stats') echo 'active' ?>"><i class="material-icons">dashboard</i>Statistics</a></li>
     <li><a href="<?= rootDashboard('product') ?>" class="waves-effect <?php if ($nav['active'] == 'product') echo 'active' ?>"><i class="material-icons">inventory_2</i>Products</a></li>
-    <li><a href="<?= rootDashboard('user') ?>" class="waves-effect <?php if ($nav['active'] == 'user') echo 'active' ?>"><i class="material-icons">group</i>Users</a></li>
+
+    <?php if ($user->role->id == 1) : ?>
+        <li><a href="<?= rootDashboard('user') ?>" class="waves-effect <?php if ($nav['active'] == 'user') echo 'active' ?>"><i class="material-icons">group</i>Users</a></li>
+    <?php endif; ?>
+
     <li>
         <div class="divider"></div>
     </li>
