9bcabc15a77ce6f41fc5e7b73a0692a76bf0923d
diolan12 <dio_lantief21@outlook.com>
2022-02-11 11:57:09 +0700

    Update testimony.php

--- resources/views/component/home/testimony.php
+++ resources/views/component/home/testimony.php
@@ -9,8 +9,8 @@
                 </div>
                 
                 <?php foreach ($data->testimonies as $testimony) : ?>
-                    <div class="col s6 m4 l3">
-                        <div class="card small">
+                    <div class="col s6 m4 l4 xl3">
+                        <div class="card">
                             <div class="card-content">
                                 <span class="card-title grey-text text-darken-4"><?= $testimony->name?></span>
                                 <blockquote><strong>&quot; <?= $testimony->quote?> &quot;</strong> <?= $testimony->country?></blockquote>
