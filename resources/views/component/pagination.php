f4cb10c23ed75503690680146d5c1b8da7cc17df
diolan12 <dio_lantief21@outlook.com>
2022-02-16 16:23:39 +0700

    Fixed pagination

--- resources/views/component/pagination.php
+++ resources/views/component/pagination.php
@@ -4,24 +4,31 @@
             background-color: <?= color($config->color->accent, false, false, true) ?> !important;
         }
     </style>
-    <div class="row">
-        <div class="col s12 center">
-            <ul class="pagination">
-                <li class="<?php if ($pagination->current == 1) echo 'disabled';
-                            else echo 'waves-effect' ?>">
-                    <a href="<?= root($pagination->path) ?>"><i class="material-icons">chevron_left</i></a>
-                </li>
-                <?php for ($i = 1; $i <= $pagination->total; $i++) : ?>
-                    <li class="<?php if ($pagination->current == $i) echo 'active';
-                                else echo 'waves-effect'; ?>">
-                        <a href="<?= root($pagination->path . '?page=' . $i) ?>"><?= $i ?></a>
+    <?php if ($pagination->total > 1) : ?>
+        <div class="row">
+            <div class="col s12 center">
+                <ul class="pagination">
+                    <li class="<?php if ($pagination->current == 1) echo 'disabled';
+                                else echo 'waves-effect' ?>">
+                        <a <?php if ($pagination->current != 1) echo 'href="' . root($pagination->path) . '"' ?>>
+                            <i class="material-icons">chevron_left</i>
+                        </a>
                     </li>
-                <?php endfor; ?>
-                <li class="<?php if ($pagination->current == $pagination->total) echo 'disabled';
-                            else echo 'waves-effect'; ?>">
-                    <a href="<?= root($pagination->path . '?page=' . $pagination->total) ?>"><i class="material-icons">chevron_right</i></a>
-                </li>
-            </ul>
+                    <?php for ($i = 1; $i <= $pagination->total; $i++) : ?>
+                        <li class="<?php if ($pagination->current == $i) echo 'active';
+                                    else echo 'waves-effect'; ?>">
+                            <a href="<?= root($pagination->path . '?page=' . $i) ?>"><?= $i ?></a>
+                        </li>
+                    <?php endfor; ?>
+                    <li class="<?php if ($pagination->current == $pagination->total) echo 'disabled';
+                                else echo 'waves-effect'; ?>">
+                        <a <?php if ($pagination->current != $pagination->total) echo 'href="' . root($pagination->path . '?page=' . $pagination->total) . '"'
+                            ?>>
+                            <i class="material-icons">chevron_right</i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
+    <?php endif; ?>
 <?php endif; ?>
