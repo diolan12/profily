2464ceba578b099234e55e1b1a3ee7d4a520746c
diolan12 <dio_lantief21@outlook.com>
2022-02-16 15:48:46 +0700

    Update products.php
    added new product modal

--- resources/views/dashboard/products.php
+++ resources/views/dashboard/products.php
@@ -5,7 +5,8 @@
         <div class="row">
             <div class="col s12">
                 <h5>Data Produk</h5>
-                <a class="waves-effect waves-light btn"><i class="material-icons left">add</i>tambah produk</a>
+                <button data-target="add-product" class="waves-effect waves-light btn modal-trigger">
+                    <i class="material-icons left">add</i>Tambah Produk</button>
             </div>
             <div class="col s12">
                 <ul class="collection">
@@ -20,6 +21,37 @@
                     <?php endforeach; ?>
                 </ul>
             </div>
+        </div>
+
+        <!-- Modal Structure -->
+        <div id="add-product" class="modal">
+            <form action="<?= rootDashboard('product') ?>" method="post" enctype="multipart/form-data">
+                <div class="modal-content">
+                    <h4>Produk Baru</h4>
+
+                    <div class="row">
+
+                        <div class="input-field col s12 m6">
+                            <input id="name" name="name" type="text" class="validate">
+                            <label for="name">Nama Produk</label>
+                        </div>
+                        <div class="input-field col s12 m6">
+                            <input id="country" name="country" type="text" class="validate">
+                            <label for="country">Negara</label>
+                        </div>
+                        <div class="input-field col s12">
+                            <textarea id="quote" name="quote" class="materialize-textarea"></textarea>
+                            <label for="quote">Quote</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn-flat waves-effect waves-light <?= color($config->color->accent, true) ?>" type="submit" name="action">
+                        Tambah
+                        <i class="material-icons left">add</i>
+                    </button>
+                </div>
+            </form>
         </div>
 
     </div>
