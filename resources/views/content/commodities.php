6abccef100ad5cb52a7f30097ec445815cf72c06
diolan12 <dio_lantief21@outlook.com>
2022-02-16 16:33:40 +0700

    Fixed data stall bug
    product not shown when product commodity is deleted

--- resources/views/content/commodities.php
+++ resources/views/content/commodities.php
@@ -1,6 +1,6 @@
 <!-- <?= component('banner', $extra) ?> -->
 
-<?= component('commodity-slider', $extra)?>
+<?= component('commodity-slider', $extra) ?>
 
 <div class="container">
     <div class="section">
@@ -8,26 +8,28 @@
         <!--   Icon Section   -->
         <div class="row">
             <div class="col s12 center">
-                <h4><?= $data->commodity->name?></h4>
-                <blockquote class="left-align"><?= $data->commodity->description1.' '.$data->commodity->description2?></blockquote>
-                <p class="left-align">Showing results for <?= $data->commodity->name?>. <a href="<?= root('product')?>">Click here</a> to see all products.</p>
+                <h4><?= $data->commodity->name ?></h4>
+                <blockquote class="left-align"><?= $data->commodity->description1 . ' ' . $data->commodity->description2 ?></blockquote>
+                <p class="left-align">Showing results for <?= $data->commodity->name ?>. <a href="<?= root('product') ?>">Click here</a> to see all products.</p>
             </div>
             <div>
                 <?php foreach ($data->products as $product) : ?>
-                    <div class="col s12 m6 l4 xl3">
-                        <div class="card medium">
-                            <div class="card-image">
-                                <img src="<?= $product->image->file ?>" class="materialboxed" data-caption="<?= $product->name ?>" alt="<?= $product->name ?>">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title"><?= $product->name ?></span>
-                                <p><?= $product->description ?></p>
-                            </div>
-                            <div class="card-action right-align">
-                                <a class="<?= color($config->color->accent, true)?>-text" href="<?= root('product/' . beauty_to_kebab($product->name)) ?>">Details</a>
+                    <?php if ($product->commodity != null && $product->type != null) : ?>
+                        <div class="col s12 m6 l4 xl3">
+                            <div class="card medium">
+                                <div class="card-image">
+                                    <img src="<?= $product->image->file ?>" class="materialboxed" data-caption="<?= $product->name ?>" alt="<?= $product->name ?>">
+                                </div>
+                                <div class="card-content">
+                                    <span class="card-title"><?= $product->name ?></span>
+                                    <p><?= $product->description ?></p>
+                                </div>
+                                <div class="card-action right-align">
+                                    <a class="<?= color($config->color->accent, true) ?>-text" href="<?= root('product/' . beauty_to_kebab($product->name)) ?>">Details</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    <?php endif; ?>
                 <?php endforeach; ?>
             </div>
 
--- resources/views/content/products.php
+++ resources/views/content/products.php
@@ -10,20 +10,22 @@
             </div>
             <div>
                 <?php foreach ($data->products as $product) : ?>
-                    <div class="col s12 m6 l4 xl3">
-                        <div class="card medium">
-                            <div class="card-image">
-                                <img src="<?= $product->image->file ?>" class="materialboxed" data-caption="<?= $product->name ?>" alt="<?= $product->name ?>">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title"><?= $product->name ?></span>
-                                <p><?= $product->description ?></p>
-                            </div>
-                            <div class="card-action right-align">
-                                <a class="<?= color($config->color->accent, true) ?>-text" href="<?= root('product/' . beauty_to_kebab($product->name)) ?>">Details</a>
+                    <?php if ($product->commodity != null && $product->type != null) : ?>
+                        <div class="col s12 m6 l4 xl3">
+                            <div class="card medium">
+                                <div class="card-image">
+                                    <img src="<?= $product->image->file ?>" class="materialboxed" data-caption="<?= $product->name ?>" alt="<?= $product->name ?>">
+                                </div>
+                                <div class="card-content">
+                                    <span class="card-title"><?= $product->name ?></span>
+                                    <p><?= $product->description ?></p>
+                                </div>
+                                <div class="card-action right-align">
+                                    <a class="<?= color($config->color->accent, true) ?>-text" href="<?= root('product/' . beauty_to_kebab($product->name)) ?>">Details</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    <?php endif; ?>
                 <?php endforeach; ?>
             </div>
         </div>
