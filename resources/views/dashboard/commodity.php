6580ad75691af03b91af96d505e32f11ac5a29d9
diolan12 <dio_lantief21@outlook.com>
2022-02-17 14:43:33 +0700

    Update commodity.php

--- resources/views/dashboard/commodity.php
+++ resources/views/dashboard/commodity.php
@@ -40,7 +40,8 @@
                             </div>
                         </div>
                         <div class="card-action right-align">
-                            <a id="delete" class="btn-flat waves-effect waves-light red-text">
+                            
+                            <a id="delete" class="btn-flat waves-effect waves-light red-text <?php if ($data->commodity->id == 1) echo 'disabled';?>">
                                 Hapus
                                 <i class="material-icons left">delete</i>
                             </a>
