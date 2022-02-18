6cd28c66c68ea1ba32e1b16336de0cb8c8c9ce2b
diolan12 <dio_lantief21@outlook.com>
2022-02-18 16:40:57 +0700

    Update lib.js

--- public/assets/js/lib.js
+++ public/assets/js/lib.js
@@ -37,6 +37,103 @@ function deleteToast() {
     let current = window.location.href.split("?")[0];
     replaceState(current);
 }
+
+class Toast {
+    constructor(msg, option = undefined) {
+        this.show = (displayLength = 4000) => {
+            M.toast({ html: msg, option: { displayLength: displayLength } });
+            return this;
+        }
+        this.dismiss = () => {
+            M.Toast.dismissAll();
+            return this;
+        }
+    }
+}
+class Cookie {
+    constructor() {
+        this.hasCookie = (name) => {
+            return this.getCookie(name) !== null;
+        }
+        this.getCookie = (name) => {
+            name += "=";
+            let decodedCookie = decodeURIComponent(document.cookie);
+            let ca = decodedCookie.split(';');
+            for (let i = 0; i < ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return null
+        }
+        this.setCookie = (name, value) => {
+            const d = new Date();
+            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+            let expires = "expires=" + d.toUTCString();
+            document.cookie = name + "=" + value + ";" + expires + ";path=/";
+            return this;
+        }
+        this.deleteCookie = (name) => {
+            this.setCookie(name, "", -1);
+            return this;
+        }
+    }
+}
+class Href {
+    constructor() {
+        const url = window.location.href;
+        const path = url.split("?")[0];
+        const param = url.split("?")[1];
+        const params = param.split("&");
+        this.hasParam = (name) => {
+            var r = false;
+            for (var i = 0; i < params.length; i++) {
+                var p = params[i].split("=");
+                if (p[0] == name) {
+                    r = true;
+                    break;
+                }
+            }
+            return r;
+        }
+        this.getParam = (name) => {
+            var r = undefined;
+            for (var i = 0; i < params.length; i++) {
+                var p = params[i].split("=");
+                if (p[0] == name) {
+                    try {
+                        r = decodeURI(p[1]);
+                    } catch (error) {
+                        console.error(error);
+                    }
+                    break;
+                }
+            }
+            return r;
+        }
+        this.clearParams = () => {
+            replaceState(path);
+            return this;
+        }
+        this.clearParam = (p = undefined) => {
+            let newUrl = path;
+            if (p == undefined) {
+                replaceState(newUrl);
+            } else if (typeof p === 'string' || p instanceof String) {
+
+            }
+            return this;
+        }
+        this.toString = () => {
+            return url;
+        }
+        return this;
+    }
+}
 class Http {
     constructor() {
         const http = new XMLHttpRequest();
@@ -100,5 +197,36 @@ class Http {
         };
     }
 }
+class Elem extends HTMLElement {
+    contructor() {
+        this.id = (id) => {
+            return document.getElementById(id)
+        }
+        this.class = (className) => {
+            return document.getElementsByClassName(className)
+        }
+    }
+}
+class App {
+    load() {
+        $.getScript("https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js", (Materialize) => {
+            eval(Materialize);
+            console.log("Materialize script loaded!");
+        });
+    }
+    constructor() {
+        this.elem = new Elem();
+        this.http = new Http();
+        this.reload = (delay = 0) => {
+            setTimeout(() => { location.reload() }, delay);
+        }
+        this.href = new Href();
+        this.cookie = new Cookie();
+        this.toast = (msg) => { return new Toast(msg) };
+        return this;
+    }
+}
+
 
 const http = new Http();
+const app = new App();
