f17ea15e588a48a35d0607b8977e97f7afae98bc
diolan12 <dio_lantief21@outlook.com>
2022-02-19 15:43:12 +0700

    Update image.php
    added image changer

--- resources/views/dashboard/image.php
+++ resources/views/dashboard/image.php
@@ -3,14 +3,15 @@
         <div class="row">
             <div class="col s12 m8 offset-m2 l6 offset-l3 xl4 offset-xl4">
                 <div class="card">
-                    <form action="<?= rootDashboard('image/' . $data->image->id) ?>" method="post" enctype="multipart/form-data">
+                    <form action="<?= rootDashboard('gallery/' . $data->image->id) ?>" method="post" enctype="multipart/form-data">
                         <div class="card-image">
                             <img src="<?= $data->image->file ?>">
                             <span class="card-title"><?= $data->image->title ?></span>
-                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">upload</i></a>
+                            <a href="#modal-upload" class="modal-trigger btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">upload</i></a>
                         </div>
                         <div class="card-content row">
                             <!-- <span class="card-title"><strong>Ubah Testimoni</strong></span> -->
+
                             <div class="input-field col s12 center">
                                 <div class="switch">
                                     <label class="tooltipped" data-position="top" data-tooltip="Tampilkan pada gallery?">
@@ -22,8 +23,8 @@
                                 </div>
                             </div>
                             <div class="input-field col s12">
-                                <input id="name" name="name" type="text" class="validate" value="<?= $data->image->title ?>">
-                                <label for="name">Judul</label>
+                                <input id="title" name="title" type="text" class="validate" value="<?= $data->image->title ?>">
+                                <label for="title">Judul</label>
                             </div>
                             <div class="input-field col s12">
                                 <textarea id="credit" name="credit" class="materialize-textarea"><?= $data->image->credit ?></textarea>
@@ -44,6 +45,32 @@
                     </form>
                 </div>
             </div>
+        </div>
+        <!-- Modal Structure -->
+        <div id="modal-upload" class="modal">
+            <form action="<?= rootDashboard('gallery/' . $data->image->id . '/img') ?>" method="post" enctype="multipart/form-data">
+                <div class="modal-content row">
+                    <h5>Upload gambar</h5>
+                    <div class="input-field col s12">
+                        <div class="file-field input-field">
+                            <div class="btn">
+                                <span>Pilih</span>
+                                <input type="file" name="image" accept="image/*" />
+                            </div>
+                            <div class="file-path-wrapper">
+                                <input name="file" class="file-path validate" type="text">
+                                <span class="helper-text">Disarankan mengupload foto yang sudah terkompres</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn-flat waves-effect waves-light <?= color($config->color->accent, true) ?>" name="action">
+                        Unggah
+                        <i class="material-icons left">upload</i>
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
     <script type="text/javascript">
