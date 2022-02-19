bf91015d430ae693e3e323de5eca93340ee80d0c
diolan12 <dio_lantief21@outlook.com>
2022-02-19 13:51:25 +0700

    Update toast.php

--- resources/views/component/toast.php
+++ resources/views/component/toast.php
@@ -1,13 +1,13 @@
 <script>
-    // Client-side toast
-    if (app.href.hasParam('toast')) {
-        app.toast(app.href.getParam('toast')).show();
-        app.href.clearParams();
-    }
-
     <?php if ($toast !== null) : ?>
         // Server-side toast
         app.toast('<?= $toast ?>').show();
         app.href.clearParams();
+    <?php else : ?>
+        // Client-side toast
+        if (app.href.hasParam('toast')) {
+            app.toast(app.href.getParam('toast')).show();
+            app.href.clearParams();
+        }
     <?php endif; ?>
 </script>
