2e1e753c888e39f76b1805daa045e0d46280b915
diolan12 <dio_lantief21@outlook.com>
2022-02-19 17:25:46 +0700

    Update commodity-slider.php

--- resources/views/component/commodity-slider.php
+++ resources/views/component/commodity-slider.php
@@ -2,14 +2,16 @@
     <ul class="slides">
         <?php shuffle($data->commodities);
         foreach ($data->commodities as $commodity) :  ?>
-            <li>
-                <img src="<?= $commodity->image->file ?>" alt="<?= $commodity->image->title ?>">
-                <div class="caption <?= random_align() ?>-align">
-                    <h3><?= $commodity->name ?></h3>
-                    <h5 class="light grey-text text-lighten-3"><?= $commodity->slogan ?></h5>
-                    <a href="<?= root('commodity/' . beauty_to_kebab($commodity->name)) ?>" class="waves-effect waves-light btn <?= color($config->color->accent) ?>">browse <?= $commodity->name ?></a>
-                </div>
-            </li>
+            <?php if ($commodity->image != null) : ?>
+                <li>
+                    <img src="<?= $commodity->image->file ?>" alt="<?= $commodity->image->title ?>">
+                    <div class="caption <?= random_align() ?>-align">
+                        <h3><?= $commodity->name ?></h3>
+                        <h5 class="light grey-text text-lighten-3"><?= $commodity->slogan ?></h5>
+                        <a href="<?= root('commodity/' . beauty_to_kebab($commodity->name)) ?>" class="waves-effect waves-light btn <?= color($config->color->accent) ?>">browse <?= $commodity->name ?></a>
+                    </div>
+                </li>
+            <?php endif; ?>
         <?php endforeach; ?>
     </ul>
 </div>
