eb91c79528e095a50b1d956ce29a7a057c658c20
diolan12 <dio_lantief21@outlook.com>
2022-02-22 15:39:51 +0700

    Update testimony.php

--- resources/views/component/home/testimony.php
+++ resources/views/component/home/testimony.php
@@ -5,7 +5,7 @@
 
             <div class="row">
                 <div class="col s12 center" data-aos="fade-up">
-                    <h4>Testimony</h4>
+                    <h4 class="font-recursive">Testimony</h4>
                 </div>
 
                 <?php $delay = 0;
@@ -13,7 +13,7 @@
                     <div class="col s12 m4 l4 xl3" data-aos="fade-up" data-aos-delay="<?= $delay ?>">
                         <div class="card">
                             <div class="card-content">
-                                <span class="card-title grey-text text-darken-4"><?= $testimony->name ?></span>
+                                <span class="card-title font-recursive"><?= $testimony->name ?></span>
                                 <blockquote><strong>&quot; <?= $testimony->quote ?> &quot;</strong> <?= $testimony->country ?></blockquote>
                             </div>
                         </div>
