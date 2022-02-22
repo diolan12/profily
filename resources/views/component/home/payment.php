0bec5f7dd4b96277877c55a6590db2291cf4ec20
diolan12 <dio_lantief21@outlook.com>
2022-02-22 15:36:54 +0700

    Update payment.php

--- resources/views/component/home/payment.php
+++ resources/views/component/home/payment.php
@@ -5,14 +5,14 @@
 
             <div class="row">
                 <div class="col s12 center" data-aos="fade-up">
-                    <h4>Payment Terms</h4>
-                    <div class="col s12 m6" data-aos="fade-right">
-                        <h5>We Do</h5>
+                    <h4 class="font-recursive">Payment Terms</h4>
+                    <div class="col s12 m6" data-aos="fade-up">
+                        <h5 class="font-recursive">We Do</h5>
                         <p><i class="material-icons left green-text">done</i>Accept T/T in advance or 100% payment</p>
                         <p><i class="material-icons left green-text">done</i>100% L/C irrevocable at sight</p>
                     </div>
-                    <div class="col s12 m6 center" data-aos="fade-right">
-                        <h5>We Do Not</h5>
+                    <div class="col s12 m6 center" data-aos="fade-up">
+                        <h5 class="font-recursive">We Do Not</h5>
                         <p><i class="material-icons left red-text">close</i>We do not accept DDP, USANCE LC, REVOCALE LC</p>
                     </div>
                 </div>
