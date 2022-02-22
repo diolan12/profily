5427886a7f1b05c585bf9e6d7774d4c17f6b375f
diolan12 <dio_lantief21@outlook.com>
2022-02-22 15:33:11 +0700

    Update commodity-slider.php

--- resources/views/component/commodity-slider.php
+++ resources/views/component/commodity-slider.php
@@ -6,8 +6,8 @@
                 <li>
                     <img src="<?= $commodity->image->file ?>" alt="<?= $commodity->image->title ?>">
                     <div class="caption <?= random_align() ?>-align">
-                        <h3><?= $commodity->name ?></h3>
-                        <h5 class="light grey-text text-lighten-3"><?= $commodity->slogan ?></h5>
+                        <h3 class="font-recursive"><?= $commodity->name ?></h3>
+                        <h5 class="light font-recursive grey-text text-lighten-3"><?= $commodity->slogan ?></h5>
                         <a href="<?= root('commodity/' . beauty_to_kebab($commodity->name)) ?>" class="waves-effect waves-light btn <?= color($config->color->accent) ?>">browse <?= $commodity->name ?></a>
                     </div>
                 </li>
